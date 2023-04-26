2d873567eb74fc113b12cb29c62e0d357116e15c
chrislng <70879967+chrislng@users.noreply.github.com>
2023-04-26 15:52:37 -0400

    Sidebar toggle event listener to modify wrapper z-index
    Set default #wrapper z-index to -1 instead of 0, so that is behind the body by default, to allow interaction with the affordances (images and download links).
    
    Added an eventListener in the javascript to modify the wrapper z-index from -1 to 0 and vice-versa when sidebarToggle is clicked.
    
    Made it so that the #wrapper transition from z-index 0 to -1 happens only after the css transition of side #sidebar-wrapper margin-left is done, using setTimeout.
    
    Added comments explaining these changes.

--- doc/js/sidebar-scripts.js
+++ doc/js/sidebar-scripts.js
@@ -11,6 +11,13 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const wrapper = document.querySelector('#wrapper');
+    
+    // These four constants serve to get the transition 
+    const sidebarWrapper = document.querySelector('#sidebar-wrapper');
+    const sidebarWrapperStyles = window.getComputedStyle(sidebarWrapper);
+    const sidebarWrapperTransitionDurationSeconds = sidebarWrapperStyles.getPropertyValue('transition-duration');
+    const sidebarWrapperTransitionDurationMilliseconds = parseFloat(sidebarWrapperTransitionDurationSeconds) * 1000;
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
@@ -20,6 +27,20 @@ window.addEventListener('DOMContentLoaded', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            /* Modify the #wrapper z-index from -1 (its default value) to 0 when #sidebarToggle is clicked, 
+                to make the sidebar visible,
+            and from 0 to -1 when #sidebarToggle is clicked again, 
+                to place #wrapper behind the body again and thus allow interaction with the page elements.
+            */
+            if (wrapper.style.zIndex === '0') {
+                /* So that the z-index goes back to -1 (behind the body) only after the #sidebar-wrapper margine-left 
+                   transition has occured. */
+                setTimeout(function() {
+                    wrapper.style.zIndex = '-1';
+                }, sidebarWrapperTransitionDurationMilliseconds);
+            } else {
+                wrapper.style.zIndex = '0';
+            }
         });
     }
 
